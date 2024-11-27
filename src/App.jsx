359db9a7f58c42eb4cc8a72571c4d367ed982ead
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-11-27 18:02:09 +0100

    Importo componenti in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+// Importo componenti
 
-import './App.css'
+import Main from './components/main/Main.jsx'
+import Footer from './components/footer/Footer.jsx'
 
 function App() {
 
   return (
     <>
-
+      <Main />
+      <Footer />
     </>
   )
 }
